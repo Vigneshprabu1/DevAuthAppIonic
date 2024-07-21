078023a4e253b83df3ba52ef8816978cd78fd022
Vigneshprabu1 <vigneshprabu479@gmail.com>
2024-07-21 15:45:38 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'devAuthApp',
+  webDir: 'www'
+};
+
+export default config;

891d3fb05b5512773ccfa28e989e10d3d88e2fb1
Vigneshprabu1 <vigneshprabu479@gmail.com>
2024-07-21 22:03:05 +0530

    add android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'devAuthApp',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 5000,
+      launchAutoHide: true,
+      androidScaleType: "CENTER_CROP",
+      splashImmersive: true,
+      backgroundColor: "#ffffff"
+    }
+  }
 };
 
 export default config;
